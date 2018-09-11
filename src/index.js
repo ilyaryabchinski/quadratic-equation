c831238f0f8038ce2a0d003495b7030dcb6699e6
Ilya <ilyaryabchinski@gmail.com>
2018-09-12 00:38:34 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  const arrayStr = equation.split(/ \* x\^2| \* x/);
+  const arrayNum = arrayStr.map(item => {
+    return +item.replace(/\s+/g, '');
+  });
+
+  const D = arrayNum[1] ** 2 - 4*arrayNum[0]*arrayNum[2];
+
+  results = [
+    Math.round(
+      (-arrayNum[1] - Math.sqrt(D)) / (2 * arrayNum[0])
+      ),
+    Math.round(
+      (-arrayNum[1] + Math.sqrt(D)) / (2 * arrayNum[0])
+      ),
+  ]
+
+  if (arrayNum[0] < 0) return results.reverse();
+  return results;
 }
